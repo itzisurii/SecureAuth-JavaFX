c5d004e29fd82eee858e5b2350b9819715dcf375
itzisurii <itzIsurika08@gmail.com>
2026-01-13 10:20:04 +0530

    feat: open registration form on register link click

--- src/main/java/edu/iCET/controllers/LoginController.java
+++ src/main/java/edu/iCET/controllers/LoginController.java
@@ -2,20 +2,32 @@ package edu.iCET.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
     @FXML
-    private PasswordField txtEmail;
+    private TextField txtEmail;
 
     @FXML
-    private TextField txtUsername;
+    private PasswordField txtPassword;
 
+    Stage stage = new Stage();
     @FXML
     void onRegisterClick(MouseEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/register_form.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
