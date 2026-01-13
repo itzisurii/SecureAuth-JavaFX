b395674633f9addd2e8d83fe7d3154fb30fc72ad
itzisurii <itzIsurika08@gmail.com>
2026-01-13 10:07:33 +0530

    feat: create LoginController to handle login form events

--- src/main/java/edu/iCET/controllers/LoginController.java
+++ src/main/java/edu/iCET/controllers/LoginController.java
@@ -1,4 +1,27 @@
 package edu.iCET.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class LoginController {
+
+    @FXML
+    private PasswordField txtEmail;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void onRegisterClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void signUpOnAction(ActionEvent event) {
+
+    }
+
 }
